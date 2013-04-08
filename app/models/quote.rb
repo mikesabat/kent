daa9bf269464de1c3ee18b00a8a2d3ce64639f17
Michael <mike.sabat@gmail.com>
2013-04-07 20:29:04 -0400

    add hero unit to stock show and boxes for quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'YahooFinance'
 
 class Quote < ActiveRecord::Base
   attr_accessible :date, :eps_actual, :eps_estimate, :neg1_close, :neg1_open, :stock_id, :time, :zero_close, :zero_open, :date_text, :period
@@ -7,17 +8,20 @@ class Quote < ActiveRecord::Base
   validates :period, :uniqueness => {:scope => :stock_id,
     :message => "One Quote Per Quarter" }
 
+  #after_create :lookup
 
 
-  # def lookup(symbol, date)
-  #     YahooFinance::get_historical_quotes( symbol,
-  #                                     date,
-  #                                     date ) do |row|
+
+  def lookup #(symbol, date)
+      YahooFinance::get_historical_quotes( stock.symbol,
+                                      date,
+                                      date ) do |row|
               
-  #             self.zero_open = row[1]
-  #             self.zero_close = row[4]        
-  #       end
-  # end
+              self.zero_open = row[1]
+              self.zero_close = row[4]        
+      end
+  end
+
   def self.grab_data(symbol)
     website = symbol
     doc = Nokogiri::HTML(open("http://www.earnings.com/company.asp?client=cb&ticker=#{website}")){|c|c.options = Nokogiri::XML::ParseOptions::NOBLANKS}
@@ -30,41 +34,26 @@ class Quote < ActiveRecord::Base
             td.text
         end
         unless release[0] == "SYMBOL" or release[4] == nil
+
           quote = Quote.new
           @stock = Stock.last #this needs to change or else it will mess something up.
-          puts "*************#{@stock.symbol}*****#{@stock.id}*******************"
+          #puts "*************#{@stock.symbol}*****#{@stock.id}*******************"
           quote.period = release[1]          
           quote.eps_estimate = release[3].delete!("$").to_f
           quote.eps_actual = release[4].delete!("$").to_f
           quote.date_text = release[6]
           quote.stock_id = "#{@stock.id}".to_i #really? How can this be this fucking complicated
-          if release[6].include? 'BMO'
+          if release[6].include? 'BMO' or release[6].include? '8:00 AM'
             quote.date = Date.parse(release[6])
-          elsif release[6].include? 'AMC'
+          elsif release[6].include? 'AMC' or release[6].include? '4:00 PM'
             quote.date = Date.parse(release[6]).next_day(1)
-          end          
+          end  
           quote.save
-          #quote.date = Time.parse(release[6])
-
-          #puts release.inspect
         end
     end
     
   end
+  
 
-
-
-  #doc.search('//b[text()="Earnings Releases"]')
-  #element = #doc.search('//b[text()="Earnings Releases"]')
-  #element.parent.parent.parent.next.next >> to locate the right table in the page
-  #doc.search('h3.r a.l', '//h3/a').each do |link|
-    #puts link.content
-
-# rows.each do |row|
-#   release = row.search("td").map do |td|
-#     td.text
-#   end
-#   puts release.inspect
-# end
 
 end
